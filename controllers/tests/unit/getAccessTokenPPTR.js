19174ecb52b46a6f11912b7cdff42fc6d0e5daec
Quentin Sahal <quentin.sahal@gmail.com>
2018-10-20 12:11:53 +0200

    now fetch a token which can be a code or an access_token depending on the request

--- controllers/tests/unit/getAccessTokenPPTR.js
+++ controllers/tests/unit/getAccessTokenPPTR.js
@@ -1,5 +1,9 @@
+// EXACTLY THE SAME AS THE SCRIPT TO GET AN AUTH CODE
+// SHOULD MERGE INTO ONE FILE
+
+
 const puppeteer = require('puppeteer');
-const {parse} = require("url");
+const qs = require("querystring");
 const debug = require("debug")("getFBCode:PPTR")
 
 const emailSelector = "#email"
@@ -8,13 +12,20 @@ const loginBtnSelector = "#loginbutton";
 const confirmPermissionsBtn = "#u_0_v button[name='__CONFIRM__']";
 const hostFB = 'www.facebook.com';
 
-const getCode = (url) => parse(url, true).query.code;
-let auth_code = "";
+const getKey = (url, key="access_token") => {
+    // On autorise que deux parties dans notre split au cas 
+    // ou il y aurait un ? et un # dans une meme url
+    // on ne prendrait pas en compte le hash
+    const dataUrl = qs.parse((url.split(/[#?]/, 2)[1]));
+    return dataUrl[key];
+}
+
+let key = "";
 
 exports.headless_auth_request = async(uri, email, pwd, redirect_uri) => {
     const browser = await puppeteer.launch({
-        // headless: false, 
-        // devtools: true
+        headless: false, 
+        devtools: true
     });
     const page = await browser.newPage();
     page.on("error", (err) => {
@@ -42,11 +53,12 @@ exports.headless_auth_request = async(uri, email, pwd, redirect_uri) => {
 
     const target_url = page.target().url()
 
-    // Il s'agit d'une premiere connection, on doit accepter les permissions
-    // on reste sur la mm page et on doit accepter les permissions
+    
     debug("target_url: ",target_url);
     debug("redirect_uri: ",redirect_uri);
 
+    // Il s'agit d'une premiere connection, on doit accepter les permissions
+    // on reste sur la mm page et on doit accepter les permissions
     if(!target_url.includes(redirect_uri)){
         console.log("We need to add the permissions");
         await page.waitFor(confirmPermissionsBtn)
@@ -56,12 +68,10 @@ exports.headless_auth_request = async(uri, email, pwd, redirect_uri) => {
         ]);
     }
     debug(page.target().url());
-
-    auth_code = getCode(page.target().url());
+    key = getKey(page.target().url());
 
     await browser.close();
 
-    return auth_code;
+    return key;
 
 }
-
