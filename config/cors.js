b279ece559655ae548269f7f3f64c7423e5ffc1e
Quentin Sahal <quentin.sahal@gmail.com>
2019-05-09 11:44:52 +0200

    fixed cors issue

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,15 @@
-const corsWhitelist = ['https://localhost:3000'];
+const secret = require("./secret");
+const corsWhitelist = secret.corsWhitelist.split(" ").join(',');
 
 module.exports = {
-    origin: corsWhitelist.join(','),
+    origin: function(origin, cb){
+        if (corsWhitelist.indexOf(origin) !== -1) {
+            cb(null, true)
+        } 
+        else {
+            cb(new Error('Not allowed by CORS'))
+        }
+    },
     credentials: true,
     maxAge: 86400,
     optionsSuccessStatus: 200
